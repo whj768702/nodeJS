74d296587ec692c503fe514e65c4c76c532a7099
whj768702 <whj768702@gmail.com>
2016-08-28 21:01:07 +0800

    创建路由文件并添加到主文件中。

---
+++ requestHandlers.js
@@ -0,0 +1,11 @@
+function start() {
+    console.log("request handler 'start' war called.");
+}
+
+function upload() {
+    console.log("request handler 'start' war called.");
+}
+
+
+exports.start = start;
+exports.upload = upload;
