933c58a81ebcf07c0fb7b131d3bda9da1ebe8f49
whj768702 <whj768702@gmail.com>
2016-08-28 21:54:30 +0800

    同时请求start、upload,不会阻塞。child_process模块。

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,21 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
     console.log("request handler 'start' war called.");
+
+    exec("ls -lah", function(error, stdout, stderr){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
     return "Hello Start";
 }
 
-function upload() {
+function upload(response) {
     console.log("request handler 'start' war called.");
-    return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received");
 
-        var content = route(handle, pathname);
+        route(handle, pathname, response);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content);
-        response.end();
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write(content);
+        // response.end();
     }
 
     http.createServer(onRequest).listen(8888);
