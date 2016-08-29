06a5e418a939fb83cb692e3fb9e7d6a877a50971
w0282 <wuhj@dtdream.com>
2016-08-29 09:22:27 +0800

    上传图片并展示。

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("request handler 'start' war called.");
 
     var body = '<html>'+
@@ -12,7 +13,7 @@ function start(response, postData) {
         '<body>'+
         '<form action="/upload" enctype="multipart/form-data" method="post">'+
         '<input type="file" name="upload">'+
-        '<input type="submit" value="Upload file" />'+
+        '<input type="submit" value="Upload file" multiple="multiple"/>'+
         '</form>'+
         '</body>'+
         '</html>';
@@ -21,15 +22,23 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("request handler 'start' war called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent: " + postData);
-    querystring.parse(postData.text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    form.uploadDir='tmp';//解决跨磁盘分区移动或操作文件会有权限问题
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.renameSync(files.upload.path, "./tmp/test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log("Request handler 'show' was called.");
     fs.readFile("./tmp/test.png", "binary", function(error, file) {
         if(error){
